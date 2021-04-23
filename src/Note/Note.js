a35ba148f63959fe89f42387d1b1ddecf2c01600
Zack Jameson <zacharymjameson@gmail.com>
2021-04-23 08:41:06 -0400

    note render update for noteful-server

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -38,11 +38,11 @@ export default class Note extends React.Component {
   };
 
   render() {
-    const { note_name, id, modified } = this.props;
+    const { name, id, modified } = this.props;
     return (
       <div className="Note">
         <h2 className="Note__title">
-          <Link to={`/note/${id}`}>{note_name}</Link>
+          <Link to={`/note/${id}`}>{name}</Link>
         </h2>
         <button
           className="Note__delete"
